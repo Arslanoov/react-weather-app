49e7a292f709a51d2a52b49c8a65aa5054845302
RasulArslanov <rasularslanoov@gmail.com>
2020-10-10 15:52:21 +0500

    First app

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+      template: 'public/index.html'
+    })
+  ],
+};
