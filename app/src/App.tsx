978a78c8bee01ab4bc77396e50fbdb77dcd45d20
Arslanoov <rasularslanoov@gmail.com>
2021-08-02 17:25:54 +0500

    feat(app): Added router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-
-import Home from './weather/pages/home';
+import React, { Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 import 'antd/dist/antd.css';
 import './assets/styles/main.scss';
 
+const Home = React.lazy(() => import('./weather/pages/home'));
+
 const App = () => (
-  <Home />
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route
+          path="/"
+          component={Home}
+          exact
+        />
+      </Switch>
+    </Suspense>
+  </Router>
 );
 
 export default App;
