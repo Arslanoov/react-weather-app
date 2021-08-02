d8fde667eb4ce560afcc8f79ea5c174c2f0366f6
Arslanoov <rasularslanoov@gmail.com>
2021-08-02 15:18:10 +0500

    refactor(app): Moved to create app template

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,91 +1,26 @@
-import * as React from 'react'
-import {
-  Switch,
-  Route,
-  Redirect,
-  BrowserRouter as Router,
-} from 'react-router-dom'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import HomePage from './common/pages/HomePage'
-import NotFoundPage from './common/pages/NotFoundPage'
-import WeatherByCityPage from './weather/pages/WeatherByCityPage'
-import IndexPage from './note/pages/IndexPage'
-import CreatePage from './note/pages/CreatePage/index'
-import ShowPage from './note/pages/ShowPage'
-import UpdatePage from './note/pages/UpdatePage/index'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-import { NoteServiceProvider } from './note/contexts/NoteServiceContext'
-import { WeatherServiceProvider } from './weather/contexts/WeatherServiceContext'
-
-import WeatherService from './weather/services/weatherService'
-// import DummyNoteService from './note/services/dummyNoteService';
-import LocalStorageNoteService from './note/services/localStorageNoteService'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-const weatherService = new WeatherService()
-const noteService = new LocalStorageNoteService()
-
-const App: React.FC = () => (
-  <WeatherServiceProvider value={weatherService}>
-    <NoteServiceProvider value={noteService}>
-      <Router>
-        <main className="main">
-          <Switch>
-            <Route
-              path="/"
-              component={HomePage}
-              exact
-            />
-
-            <Route
-              path="/weather"
-              component={WeatherByCityPage}
-              exact
-            />
-
-            <Route
-              path="/notes"
-              component={IndexPage}
-              exact
-            />
-
-            <Route
-              path="/create"
-              component={CreatePage}
-              exact
-            />
-
-            <Route
-              path="/note/update/:id"
-              render={({ match }) => {
-                const { id } = match.params
-                return <UpdatePage id={id} />
-              }}
-            />
-
-            <Route
-              path="/note/:id"
-              render={({ match }) => {
-                const { id } = match.params
-                return <ShowPage id={id} />
-              }}
-            />
-
-            <Route
-              path="/404"
-              component={NotFoundPage}
-              exact
-            />
-
-            <Route>
-              <Redirect to="/404" />
-            </Route>
-          </Switch>
-        </main>
-      </Router>
-    </NoteServiceProvider>
-  </WeatherServiceProvider>
-)
-
-export default App
+export default App;
