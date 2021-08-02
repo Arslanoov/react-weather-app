205b994f5e5a55245e3242e27364f466e1f0ce50
Arslanoov <rasularslanoov@gmail.com>
2021-08-02 15:33:55 +0500

    feat(app): Installed Ant Design

---
+++ app/src/weather/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import WeatherLayout from '../../layouts/weather-layout';
+
+const Home = () => {
+  return (
+    <WeatherLayout>
+      Home page
+    </WeatherLayout>
+  );
+};
+
+export default Home;
