b3db076f84b6df2e20b6ad3c765790c5c99df0ec
Arslanoov <rasularslanoov@gmail.com>
2021-08-03 23:32:32 +0500

    refactor(components): Changed components structure

---
+++ app/src/weather/components/test/Test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test = () => (
+  <div>Test Page</div>
+);
+
+export default Test;
--- app/src/weather/layouts/weather-layout/index.tsx
+++ app/src/weather/layouts/weather-layout/index.tsx
@@ -1,20 +1,3 @@
-import React from 'react';
-
-import { Layout } from 'antd';
-
-import Header from 'weather/layouts/header';
-import Footer from 'weather/layouts/footer';
-import Sidebar from 'weather/layouts/sidebar';
-
-const WeatherLayout: React.FC = ({ children }) => (
-  <Layout>
-    <Header />
-    <Layout>
-      <Sidebar />
-      <Layout.Content style={{ padding: '10px 20px' }}>{children}</Layout.Content>
-    </Layout>
-    <Footer />
-  </Layout>
-);
+import WeatherLayout from './WeatherLayout';
 
 export default WeatherLayout;
