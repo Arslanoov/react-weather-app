2d84110af618d103c1fd19dea8684159ed009326
Arslanoov <rasularslanoov@gmail.com>
2021-08-03 12:04:00 +0500

    feat(store): Connected store to component

--- app/src/weather/pages/home/index.tsx
+++ app/src/weather/pages/home/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
 import WeatherLayout from '../../layouts/weather-layout';
+import { AppDispatch, RootState } from '../../../store';
+import { citySelector, fetchCity as fetchCityAction } from '../../../store/slices/weather';
 
-const Home = () => (
-  <WeatherLayout>
-    <div className="home">Home page</div>
-  </WeatherLayout>
-);
+type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps> & {};
 
-export default Home;
+const Home: React.FC<Props> = ({ city, fetchCity }) => {
+  useEffect(() => {
+    fetchCity();
+  }, []);
+
+  return (
+    <WeatherLayout>
+      <div className="home">Home page</div>
+      <div>{city}</div>
+    </WeatherLayout>
+  );
+};
+
+const mapStateToProps = (state: RootState) => ({
+  city: citySelector(state),
+});
+
+const mapDispatchToProps = (dispatch: AppDispatch) => ({
+  fetchCity: () => dispatch(fetchCityAction()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
