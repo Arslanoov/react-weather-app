5310c688bbfa3cead7d82b22405d60ff16acbc76
Arslanoov <rasularslanoov@gmail.com>
2021-09-11 15:42:38 +0500

    test(weather): Fixed current tests

--- app/src/weather/components/presentational/weather-card-row/WeatherCardRow.test.tsx
+++ app/src/weather/components/presentational/weather-card-row/WeatherCardRow.test.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import { screen, render } from '@testing-library/react';
 import userEvent, { TargetElement } from '@testing-library/user-event';
+
+import * as themeSwitcher from 'react-css-theme-switcher';
 
 import weatherData from 'tests/dummy/weatherData';
 import WeatherCardRow from './WeatherCardRow';
 
 describe('<WeatherCardRow />', () => {
   it('renders correctly', () => {
-    render(<WeatherCardRow data={weatherData} onDelete={() => {}} />);
+    const useThemeSwitcherSpy = jest
+      .spyOn(themeSwitcher, 'useThemeSwitcher')
+      .mockReturnValue({
+        theme: 'dark',
+      } as unknown as ReturnType<typeof themeSwitcher.useThemeSwitcher>);
+
+    render(
+      <Router>
+        <WeatherCardRow data={weatherData} onDelete={() => {}} />
+      </Router>,
+    );
+
+    expect(useThemeSwitcherSpy).toBeCalled();
 
     const cityName = screen.getByText(weatherData.name);
     expect(cityName).toBeInTheDocument();
   });
 
   test('deleting item', () => {
+    const useThemeSwitcherSpy = jest
+      .spyOn(themeSwitcher, 'useThemeSwitcher')
+      .mockReturnValue({
+        theme: 'dark',
+      } as unknown as ReturnType<typeof themeSwitcher.useThemeSwitcher>);
+
     const onDeleteSpy = jest.fn();
-    const { container } = render(<WeatherCardRow data={weatherData} onDelete={onDeleteSpy} />);
+    const { container } = render(
+      <Router>
+        <WeatherCardRow data={weatherData} onDelete={onDeleteSpy} />
+      </Router>,
+    );
+
+    expect(useThemeSwitcherSpy).toBeCalled();
 
     const close = container.querySelector('.weather-card-row__delete');
 
